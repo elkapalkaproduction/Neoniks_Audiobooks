40f616afb4cd9db1c4f9934796941c50302078d7
Andrei Vidrasco <Andrei@102.1.168.192.in-addr.arpa>
2014-03-21 21:04:31 +0200

    Initial Commit

---
+++ audiobooks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  audiobooks
+//
+//  Created by Andrei Vidrasco on 3/21/14.
+//  Copyright (c) 2014 Andrei Vidrasco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ audiobooks/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  audiobooks
+//
+//  Created by Andrei Vidrasco on 3/21/14.
+//  Copyright (c) 2014 Andrei Vidrasco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
